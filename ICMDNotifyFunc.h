9fd11f181c67b7413dd4bd150a4d3ffbcc481729
ShunmingGuo <631550367@qq.com>
2019-02-18 16:34:19 +0800

    添加回调声明辅组接口

---
+++ ICMDNotifyFunc.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <string>
+
+template<typename TTCmd>
+class ICMDNotifyFunc
+{
+public:
+    virtual ~ICMDNotifyFunc() {}
+
+    virtual bool notify(const std::string& actionName, const TTCmd* pcmd) const = 0;
+};
